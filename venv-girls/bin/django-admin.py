1936d71a641100fbcf0982df39b8c4674a92fe5a
KimISeob <seobew@gmail.com>
2016-12-26 17:13:05 +0900

    first commit

---
+++ venv-girls/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kimiseob/Documents/workspace/django/djangogirls/venv-girls/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
